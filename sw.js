89f066d5f26aedafa76adf275d2b5f3f417f8947
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-04-21 20:13:21 -0300

    teste 2 de paginas offline

--- sw.js
+++ sw.js
@@ -1,28 +1,36 @@
-// This is the "Offline page" service worker
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
+const staticCacheName = 'site-static';
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+const offlinePages = [
+    '/',
+    'index.html',
+    'home.html',
+    'vendor/bootstrap/css/bootstrap.min.css',
+    'css/signin.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'icons/icon-96x96',
+     'app.js',
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+     'vendor/jquery/jquery.min.js',
+     'vendor/bootstrap/js/bootstrap.bundle.min.js',
+         'css/modern-business.css',
+];
 
 self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
+ event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+        cache.addAll(offlinePages)
+    })
+ );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+
+
+self.addEventListener('activate', (event) => {
+
+
+});
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
@@ -38,8 +46,8 @@ self.addEventListener('fetch', (event) => {
         return networkResp;
       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
+        const cache = await caches.open(staticCacheName);
+        const cachedResp = await cache.match(offlinePages);
         return cachedResp;
       }
     })());
